8673481154c7e10541a946b884ff128ca0cff339
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-13 15:50:48 -0300

    Fixing the broken notice list

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,132 +1,165 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
-import {View, Text, Image, TextInput, FlatList, TouchableOpacity, KeyboardAvoidingView, Platform, Keyboard, Alert, AsyncStorage} from 'react-native'
-import {MaterialIcons} from '@expo/vector-icons'
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  Alert,
+  AsyncStorage,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import styles from './styles'
+import styles from './styles';
 
-export default function Feed(){
+export default function Feed() {
+  // troquei para as notíficas serem uma lista
+  const [notices, setNotices] = useState([]);
+  // As notificas agora são um objeto
+  const [newNotice, setNewNotice] = useState({
+    title: '',
+    news: '',
+    author: '',
+  });
 
-    const [notices, setNotices] = useState({
-        title: "",
-        news: "",
-        author: ""
-    });
-    const [newNotices, setNewNotices] = useState('');
-
-    async function addNotice() {
-
-        if(newNotices === ""){
-            return;
-        }
-
-        const search = notices.filter(notices => notices === newNotices);
-
-        if(search.length !== 0) {
-            Alert.alert("Atenção", "Essa notícia está repetida");
-            return;
-        }
-
-
-        setNotices([...notices, newNotices]);
-        setNewNotices('');
-
-        Keyboard.dismiss();
-
-
-    } 
-
-
-    async function removeNotice(item){
-
-        Alert.alert("Deletar notícia",
-                    "Tem certeza que deseja remover essa notícia?",
-                    [
-                        {
-                            text: "Cancelar", 
-                            onPress: () => {
-                                return;
-                            },
-                            style: 'cancel'
-                        },
-                        {
-                            text: "Confirmar",
-                            onPress: () => setNotices(notices.filter(notices => notices !== item ))
-                        }
-                    ]
-        )
-
+  // não precisa de async porque não tem nenhum await na função
+  function addNotice() {
+    if (newNotice.title === '') {
+      return;
     }
 
-    useEffect(() => {
-        async function carregaDados(){
-            const notices = await AsyncStorage.getItem("notices");
+    // Essa é uma validação complicada de ser feita.
+    // O que eu aconselharia fazer aqui seria comparar apenas o titulo para não ser repetido
+    // Vou deixar um exemplo comentado abaixo
+    // const search = notices.filter(notice => notice.title === newNotice.title);
+    const search = notices.filter(notice => notice === newNotice);
 
-            if(notices){
-                setNotices(JSON.parse(notices))
-            }
-        }
+    if (search.length !== 0) {
+      Alert.alert('Atenção', 'Essa notícia está repetida');
+      return;
+    }
 
-        carregaDados();
-    }, [])
+    setNotices([...notices, newNotice]);
+    setNewNotice({
+      title: '',
+      news: '',
+      author: '',
+    });
 
-    useEffect(() => {
-        async function salvarDados(){
-            AsyncStorage.setItem("notices", JSON.stringify(notices))
-        }
+    Keyboard.dismiss();
+  }
 
-        salvarDados();
-    }, [notices, ])
+  // não precisa de async porque não tem nenhum await na função
+  function removeNotice(item) {
+    Alert.alert(
+      'Deletar notícia',
+      'Tem certeza que deseja remover essa notícia?',
+      [
+        {
+          text: 'Cancelar',
+          onPress: () => {
+            return;
+          },
+          style: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          onPress: () => {
+            //aqui estarão as notificar filtradas (por boa prática é melhor separar cada coisa no codigo)
+            // As notíficas serão filtradas por titulo, pois cada notífica deve ter um titulo diferente
+            // Seria bom se você desse um ID para cada notifica, assim seria mais facil remover
+            const filteredNotices = notices.filter(
+              notice => notice.title !== item.title
+            );
+            setNotices([...filteredNotices]);
+            // return; // talvez falte um return aqui
+          },
+        },
+      ]
+    );
+  }
 
-    return(
-     <KeyboardAvoidingView
-        keyboardVerticalOffset={0}
-        behavior="padding"
-        style={{flex: 1}}
-        enabled={Platform.OS === 'ios'}
-     >
-        <View style={styles.container}>
-            <Image style={styles.logo} source={{uri: 'https://media.glassdoor.com/sqll/1048135/flex-contact-center-squarelogo-1576647816123.png'}}/>
+  useEffect(() => {
+    async function carregaDados() {
+      const loadeNnotices = await AsyncStorage.getItem('notices');
 
-            <View style={styles.search}>
-                <TextInput
-                    placeholder="Procurar notícias"
-                    style={styles.inputSearch}
-                />
-            </View>
+      if (notices) {
+        setNotices(JSON.parse(loadeNnotices));
+      }
+    }
 
-            <FlatList
-                style={styles.notices}
-                data={notices}
-                keyExtractor={item => item.toString()}
-                showsVerticalScrollIndicator={false}
-                renderItem={({item}) => (
-                    <View style={styles.mainNotices}>
-                        <Text>{item}</Text>
-                        <TouchableOpacity
-                            onPress={() => removeNotice(item)}
-                        >
-                            <MaterialIcons name="delete-forever" size={20} color="red"/>
-                        </TouchableOpacity>
-                    </View>
-                )}
-            />
+    carregaDados();
+  }, []);
 
-            <View style={styles.form}>
-                <Text style={styles.formTextCall}>Adicione sua notícia</Text>
-                <TextInput style={styles.formName} onChangeText={text => setNewNotices(text)} value={newNotices}placeholder="Título da notícia"/>
-                <TextInput style={styles.formName} placeholder="Autor"/>
-                <TextInput style={styles.formText} placeholder="Digite sua notícia"/>
-                <TouchableOpacity
-                    style={styles.buttonMain}
-                    onPress={() => addNotice()}
-                >
-                    <Text style={styles.buttonForm}>Enviar</Text>
-                </TouchableOpacity>
-            </View>
+  useEffect(() => {
+    async function salvarDados() {
+      AsyncStorage.setItem('notices', JSON.stringify(notices));
+    }
 
+    salvarDados();
+  }, [notices]);
 
+  return (
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={0}
+      behavior="padding"
+      style={{ flex: 1 }}
+      enabled={Platform.OS === 'ios'}
+    >
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri:
+              'https://media.glassdoor.com/sqll/1048135/flex-contact-center-squarelogo-1576647816123.png',
+          }}
+        />
+
+        <View style={styles.search}>
+          <TextInput
+            placeholder="Procurar notícias"
+            style={styles.inputSearch}
+          />
         </View>
-     </KeyboardAvoidingView>  
-    )
+
+        <FlatList
+          style={styles.notices}
+          data={notices}
+          keyExtractor={item => item.toString()}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={styles.mainNotices}>
+              <Text>{item}</Text>
+              <TouchableOpacity onPress={() => removeNotice(item)}>
+                <MaterialIcons name="delete-forever" size={20} color="red" />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+
+        <View style={styles.form}>
+          <Text style={styles.formTextCall}>Adicione sua notícia</Text>
+          <TextInput
+            style={styles.formName}
+            onChangeText={text => setNewNotice(text)}
+            value={newNotice}
+            placeholder="Título da notícia"
+          />
+          <TextInput style={styles.formName} placeholder="Autor" />
+          <TextInput style={styles.formText} placeholder="Digite sua notícia" />
+          <TouchableOpacity
+            style={styles.buttonMain}
+            onPress={() => addNotice()}
+          >
+            <Text style={styles.buttonForm}>Enviar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
